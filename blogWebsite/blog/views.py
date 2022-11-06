8e39b5a242173937261e2712b346710c49ce5be5
bakhet <baketalrwes@gmail.com>
2022-11-06 14:37:19 +0300

    ORM

--- blogWebsite/blog/views.py
+++ blogWebsite/blog/views.py
@@ -5,13 +5,14 @@ from . models import Bolg
 def add_blog(request: HttpRequest):
 
     if request.method == "POST":
-        new_book = Bolg(title=request.POST["title"], Content= request.POST["Content"], is_published= request.POST["is_published"], publish_date= request.POST["publish_date"])
-        new_book.save()
+        new_blog = Bolg(title=request.POST["title"], Content= request.POST["Content"], is_published= request.POST["is_published"], publish_date= request.POST["publish_date"])
+        new_blog.save()
         
 
     return render(request, "blog/addblog.html")
 
-def list_blog(request:HttpRequest):
-    books_list = Bolg.objects.all()
 
-    return render(request, "blog/raedblog.html", {"blog" : books_list})
+def list_blog(request: HttpRequest):
+    blog_list = Bolg.objects.all()
+   
+    return render(request, "blog/readblog.html", {"blog_list" : blog_list})
